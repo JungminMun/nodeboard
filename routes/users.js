101ebc476c4d6d3abc5f4185f92fb73a4d1f9aee
SEXYJungmin <jmdev@kakao.com>
2021-01-10 00:45:37 +0900

    Update : Login Update

--- routes/users.js
+++ routes/users.js
@@ -3,108 +3,82 @@ const router = express.Router();
 const User = require('../models/User');
 const util = require('../util');
 
-router.get('/', (req, res) => {
-    User.find({})
-        .sort({
-            username:1 // 오름차순 정렬
-        })
-        .exec((err, users) =>{
-            if(err){
-                return res.json(err);
-            }
-            res.render('users/index', {users:users});
-        });
+// New
+router.get('/create', function(req, res){
+  var user = req.flash('user')[0] || {};
+  var errors = req.flash('errors')[0] || {};
+  res.render('users/create', { user:user, errors:errors });
 });
 
-router.get('/create', (req, res) => {
-    var user = req.flash('user')[0] || {};
-    var errors = req.flash('errors')[0] || {};
-    res.render('users/create', { user:user, errors:errors });  
-});
-
-router.post('/', (req, res) => {
-    User.create(req.body, (err, user) => {
-        if(err){
-            req.flash('user', req.body);
-            req.flash('errors', util.parseError(err)); // 1
-            return res.redirect('/users/new');
-        }
-        res.redirect('/users');
-    });
-});
-  
-
-router.get('/:username', (req, res) => {
-    var user = req.flash('user')[0];
-    var errors = req.flash('errors')[0] || {};
-
-    if(!user){
-        User.findOne({username:req.params.username}, (err, user) =>{
-            if(err){
-                return res.json(err);
-            }
-            res.render('users/edit', { username:req.params.username, user:user, errors:errors });
-        });
+// create
+router.post('/', function(req, res){
+  User.create(req.body, function(err, user){
+    if(err){
+      req.flash('user', req.body);
+      req.flash('errors', util.parseError(err));
+      return res.redirect('/users/create');
     }
-    else {
-        res.render('users/edit', { username:req.params.username, user:user, errors:errors });
-    }
- 
+    res.redirect('/');
+  });
 });
 
-router.get('/:username/edit', (req, res) => {
-    User.findOne({username:req.params.username}, (err, user) => {
-        if(err){
-            return res.json(err);
-        }
-        res.render('users/edit', {user:user});
+// show
+router.get('/:username', util.isLoggedin, checkPermission, function(req, res){
+  User.findOne({username:req.params.username}, function(err, user){
+    if(err) return res.json(err);
+    res.render('users/show', {user:user});
+  });
+});
+
+// edit
+router.get('/:username/edit', util.isLoggedin, checkPermission, function(req, res){
+  var user = req.flash('user')[0];
+  var errors = req.flash('errors')[0] || {};
+  if(!user){
+    User.findOne({username:req.params.username}, function(err, user){
+      if(err) return res.json(err);
+      res.render('users/edit', { username:req.params.username, user:user, errors:errors });
     });
+  }
+  else {
+    res.render('users/edit', { username:req.params.username, user:user, errors:errors });
+  }
 });
 
-router.put('/:username', function(req, res, next){
-    User.findOne({username:req.params.username})
-        .select('password')
-        .exec(function(err, user){
-            if(err){
-                return res.json(err);
-            }
-            user.save(function(err, user){
-                if(err){
-                    req.flash('user', req.body);
-                    req.flash('errors', util.parseError(err));
-                    return res.redirect('/users/'+req.params.username+'/edit'); // 1
-                }
-                res.redirect('/users/'+user.username);
-            });
-        });
-});
-  
+// update
+router.put('/:username', util.isLoggedin, checkPermission, function(req, res, next){
+  User.findOne({username:req.params.username})
+    .select('password')
+    .exec(function(err, user){
+      if(err) return res.json(err);
 
-router.delete('/:username', (req, res) =>{
-    User.deleteOne({username:req.params.username}, (err) => {
+      // update user object
+      user.originalPassword = user.password;
+      user.password = req.body.newPassword? req.body.newPassword : user.password;
+      for(var p in req.body){
+        user[p] = req.body[p];
+      }
+
+      // save updated user
+      user.save(function(err, user){
         if(err){
-            return res.json(err);
+          req.flash('user', req.body);
+          req.flash('errors', util.parseError(err));
+          return res.redirect('/users/'+req.params.username+'/edit');
         }
-        res.redirect('/users');
-    });
+        res.redirect('/users/'+user.username);
+      });
+  });
 });
 
 module.exports = router;
 
-parseError = (errors) => {
-    var parsed = {};
-    if(errors.name == 'ValidationError'){
-        for(var name in errors.errors){
-            var validationError = errors.errors[name];
-            parsed[name] = { message:validationError.message };
-        }
-    }
-    else if(errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
-        parsed.username = { message:'This username already exists!' };
-    }
-    else {
-        parsed.unhandled = JSON.stringify(errors);
-    }
-    return parsed;
+// private functions
+function checkPermission(req, res, next){
+  User.findOne({username:req.params.username}, function(err, user){
+    if(err) return res.json(err);
+    if(user.id != req.user.id) return util.noPermission(req, res);
+
+    next();
+  });
 }
-  
