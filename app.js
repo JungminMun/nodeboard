19fed1c4cfd7083d39396e4758e97c3ca8a34313
SEXYJungmin <jmdev@kakao.com>
2021-01-09 21:29:36 +0900

    Update : 회원가입을 위한 user routes 추가

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 const mongooseConnect = require('./models/mongoose');
 const app = express();
 
@@ -10,9 +12,17 @@ app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(flash()); 
+app.use(session({
+    secret:'MySecret', 
+    resave:true, 
+    saveUninitialized:true
+})); 
+
 
 app.use('/', require('./routes/home'));
-app.use('/main', require('./routes/posts')); // 1
+app.use('/main', require('./routes/posts'));
+app.use('/users', require('./routes/users'));
 
 app.listen(3000, () => {
     console.log('Connected ! https://localhost:3000');
