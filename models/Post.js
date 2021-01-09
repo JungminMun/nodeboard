5ba83cbe62597bff768f9a04203ae085ac8cbfb1
SEXYJungmin <jmdev@kakao.com>
2021-01-09 21:35:06 +0900

    Update : Posts rescue

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-// schema
-var postSchema = mongoose.Schema({ // 1
-  title:{type:String, required:true},
-  body:{type:String, required:true},
-  createdAt:{type:Date, default:Date.now}, // 2
-  updatedAt:{type:Date},
+var postSchema = mongoose.Schema({ 
+    title : {
+        type : String, 
+        required : true
+    }, body : { 
+        type : String, 
+        required : true
+    }, createdAt : {
+        type : Date, 
+        default : Date.now
+    }, updatedAt : {
+        type : Date
+    }
 });
 
-// model & export
 var Post = mongoose.model('post', postSchema);
 module.exports = Post;
