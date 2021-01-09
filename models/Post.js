90a30cbfe83871bf308755df5cc37ab9319dff78
SEXYJungmin <jmdev@kakao.com>
2021-01-09 23:17:08 +0900

    Update : author model update

--- models/Post.js
+++ models/Post.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose');
 var postSchema = mongoose.Schema({ 
     title : {
         type : String, 
-        required : true
+        required:[true,'Title is required!']
     }, body : { 
         type : String, 
-        required : true
+        required:[true,'Body is required!']
     }, createdAt : {
         type : Date, 
         default : Date.now
+        
+    }, author:{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'user', 
+        required:true
     }, updatedAt : {
         type : Date
-    }
+    }, 
 });
 
 var Post = mongoose.model('post', postSchema);
